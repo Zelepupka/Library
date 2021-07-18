2296793294cc2aee5b01a774d316d078d6221dd7
Zelepupka <cesenko132@gmail.com>
2021-07-18 16:39:52 +0700

    Done GenreDatatTable

---
+++ Library.Web/wwwroot/js/initTable.js
@@ -0,0 +1,24 @@
+﻿
+function InitTable() {
+    $(document).ready(function () {
+        $("#genre-table").DataTable({
+            "processing": true,
+            "serverSide": true,
+            "filter": true,
+            "orderMulti": false,
+            "pageLength": 5,
+            "ajax": {
+                "url": "/Genres/LoadData",
+                "type": "POST",
+                "datatype": "json"
+            },
+
+            "columns": [
+                { "data": "id", "name": "Id", "autoWidth": true },
+                { "data": "name", "name": "Name", "autoWidth": true }
+            ]
+
+        });
+    });
+}
+
