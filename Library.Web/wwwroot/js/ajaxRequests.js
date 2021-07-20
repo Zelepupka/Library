052eb15af45c91099c8a9b107f34a82bf798b660
Zelepupka <cesenko132@gmail.com>
2021-07-20 23:18:02 +0700

    AjaxRequests Files

---
+++ Library.Web/wwwroot/js/ajaxRequests.js
@@ -0,0 +1,39 @@
+﻿function AjaxGet() {
+        $.ajax({
+            type: "GET",
+            url: '/Genres/Add',
+            dataType: 'html',
+            success: function(data) {
+                console.log(data);
+                $('#formBody').html(data);
+                $('#formBody').show();
+            }
+        });
+
+}
+
+function AjaxPost() {
+        $.ajax({
+            url: '/Genres/Edit',
+            type: 'POST',
+            data: {
+                Id: document.getElementById('idBox').textContent,
+                Name: document.getElementById('nameBox').textContent
+            },
+            success: function() {
+               
+            }
+        });
+}
+
+function AjaxDelete(id) {
+        $.ajax({
+            url: '/Genres/Delete',
+            type: 'GET',
+            data: id,
+            success: function() {
+               
+            }
+        });
+}
+  
